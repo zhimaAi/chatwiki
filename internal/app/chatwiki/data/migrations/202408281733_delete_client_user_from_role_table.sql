1b36a9b4b4db1f7a749e49e24b974c9d719e4ddb
lion <921405104@qq.com>
2024-09-11 16:45:01 +0800

    fix: sql bug

--- internal/app/chatwiki/data/migrations/202408281733_delete_client_user_from_role_table.sql
+++ internal/app/chatwiki/data/migrations/202408281733_delete_client_user_from_role_table.sql
@@ -1,4 +1,4 @@
 -- +goose Up
 
 DELETE FROM "public"."role" WHERE role_type = 4;
-UPDATE "public"."user" SET user_roles = (SELECT id FROM "public"."role" WHERE role_type = 1) WHERE user_name = 'admin'
+UPDATE "public"."user" SET user_roles = (SELECT id FROM "public"."role" WHERE role_type = 1) WHERE user_name = 'admin';
