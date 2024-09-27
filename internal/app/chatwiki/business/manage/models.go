9c63b0b638b68de357d8a18e4f15e9ca93963a98
lion <921405104@qq.com>
2024-09-27 17:09:43 +0800

    build: pack

--- internal/app/chatwiki/business/manage/models.go
+++ internal/app/chatwiki/business/manage/models.go
@@ -106,7 +106,7 @@ func AddModelConfig(c *gin.Context) {
 	err := configurationTest(config, modelInfo)
 	if err != nil {
 		logs.Error(err.Error())
-		c.String(http.StatusOK, lib_web.FmtJson(nil, errors.New(i18n.Show(common.GetLang(c), err.Error()))))
+		c.String(http.StatusOK, lib_web.FmtJson(nil, err))
 		return
 	}
 
@@ -235,7 +235,7 @@ func EditModelConfig(c *gin.Context) {
 	err = configurationTest(info, modelInfo)
 	if err != nil {
 		logs.Error(err.Error())
-		c.String(http.StatusOK, lib_web.FmtJson(nil, errors.New(i18n.Show(common.GetLang(c), err.Error()))))
+		c.String(http.StatusOK, lib_web.FmtJson(nil, err))
 		return
 	}
 
