d588489b0dff1b19907c05059b3a7b132f0b38c9
lion <921405104@qq.com>
2025-05-21 11:03:04 +0800

    fix: 修复数据库升级文件版本号错误问题

--- internal/app/chatwiki/data/migrations/20250509_alter_table_library_search.sql
+++
@@ -1,6 +0,0 @@
--- +goose Up
-
-ALTER TABLE "user_search_config" ADD COLUMN "prompt_type" int4 NOT NULL DEFAULT 0;
-ALTER TABLE "user_search_config" ADD COLUMN "prompt" varchar(500) NOT NULL DEFAULT '';
-
-COMMENT ON COLUMN "user_search_config"."prompt" IS '提示词';
